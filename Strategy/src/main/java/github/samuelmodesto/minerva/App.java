53d59819879dcfd398f4f064c14cf72764690c5a
Samuel Modesto <samuelmodestoes@gmail.com>
2022-07-20 19:33:02 -0300

    fix class

--- Strategy/src/main/java/github/samuelmodesto/minerva/App.java
+++ Strategy/src/main/java/github/samuelmodesto/minerva/App.java
@@ -4,9 +4,7 @@ import github.samuelmodesto.minerva.interfaces.impl.AtaqueComEspada;
 import github.samuelmodesto.minerva.interfaces.impl.AtaqueComFlechasEnvenenadas;
 import github.samuelmodesto.minerva.interfaces.impl.AtaqueComFogo;
 import github.samuelmodesto.minerva.model.Guerreiro;
-import lombok.extern.log4j.Log4j2;
 
-@Log4j2
 public class App {
     public static void main(String[] args) {
         System.out.println("Um lycan apareceu!");
@@ -15,7 +13,6 @@ public class App {
         System.out.println("Um Monstro de gelo apareceu!");
         guerreiro.executarAtaque(new AtaqueComFogo());
         System.out.println("Um Vampiro apareceu!");
-        guerreiro.executarAtaque(new AtaqueComFlechasEnvenenadas()); 
-
+        guerreiro.executarAtaque(new AtaqueComFlechasEnvenenadas());
     }
 }
