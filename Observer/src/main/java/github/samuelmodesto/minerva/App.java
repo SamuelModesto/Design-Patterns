9896bfbb432b28ce7e1931c5c206d31122f89e3b
Samuel Modesto <samuelmodestoes@gmail.com>
2023-01-04 11:10:37 -0300

    resolvendo conflito

--- Observer/src/main/java/github/samuelmodesto/minerva/App.java
+++ Observer/src/main/java/github/samuelmodesto/minerva/App.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Observer/src/main/java/github/samuelmodesto/minerva/App.java
 package github.samuelmodesto.minerva;
 
 public class App {
@@ -6,12 +5,4 @@ public class App {
         System.out.println("Padrao Observer iniciado");
     }
 }
-=======
-package github.samuelmodesto.minerva;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Padrao Command iniciado");
-    }
-}
->>>>>>> a7f1d2304aafddf802cee6f91cc4b93c7f2da13a:Command/src/main/java/github/samuelmodesto/minerva/App.java
